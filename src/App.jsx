5fb6cbbfe6659998a4fd7512fbc05aa4919f0a07
ezinne favour <ezinnefavou@gmail.com>
2026-08-23 12:35:43 +0100

    Display users from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Fetchdata from "./Components/Fetchdata"
+
 import Userlist from "./Components/Userlists"
 function App() {
  
 
   return (
     <>
-     <Fetchdata />
-     <Userlist />
+     
     </>
   )
 }
